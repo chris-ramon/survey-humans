f8b1b6ad54811df13f210653b67f73279d726eae
Eduardo Arenas <adbeel_eduardo@hotmail.com>
2012-09-06 14:48:36 -0500

    upd and new: cruds for admin app

---
+++ db/seeds/12_data_init.seeds.rb
@@ -0,0 +1,7 @@
+#MatchType.create({:name=>"Survey"})
+#MatchType.create({:name=>"Exam"})
+
+QuestionLevel.create({:range=>"Easy",:deleted=>0})
+QuestionLevel.create({:range=>"Medium",:deleted=>0})
+QuestionLevel.create({:range=>"Hard",:deleted=>0})
+QuestionLevel.create({:range=>"Genius",:deleted=>0})
