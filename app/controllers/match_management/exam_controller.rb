600ccd4dff70977906dedd27f9d66f5dcec12483
Eduardo Arenas <adbeel_eduardo@hotmail.com>
2012-11-30 10:57:24 -0500

    students graph

--- app/controllers/match_management/exam_controller.rb
+++ app/controllers/match_management/exam_controller.rb
@@ -18,6 +18,18 @@ class MatchManagement::ExamController < ApplicationController
       @list_question_levels = QuestionLevel.where(:deleted=>0)
       @list_courses = Course.where(:user_id=>current_user.id,:deleted=>0).order("name")
       @list_match_types = MatchType.where(:deleted=>0)
+      @panel_id=Panel::Subscription.where(:user_id=>current_user.id).first.panel_plan_id
+      @can_add=false
+      matches=Match.where(:deleted => 0, :user_id=>current_user.id)
+      case @panel_id
+        when 1
+          @can_add=(matches.count<5)
+        when 2
+          @can_add=(matches.count<10)
+        else
+          @can_add=true
+      end
+      @plan=Panel::Plan.find(@panel_id).name
       respond_to do |format|
         format.html
         format.js
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,12 @@
 class Student < ActiveRecord::Base
 	belongs_to :course
+
+	def self.can_see_graph(course)
+		students=Student.where(:course_id=>course.id)
+		can=true
+		students.each do |st|
+			can=false if st.corrects==nil
+		end
+		can
+	end
 end
